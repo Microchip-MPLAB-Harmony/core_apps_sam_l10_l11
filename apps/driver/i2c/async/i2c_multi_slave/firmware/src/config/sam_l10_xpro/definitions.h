c14a19ee796788824b6d17a56aca523ee73fe20a
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-02 22:59:19 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_async_i2c_multi_slave_sam_l10_xpro
    MH3-50717
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_l10_xpro/definitions.h
@@ -52,10 +52,10 @@
 #include "peripheral/sercom/i2c_master/plib_sercom1_i2c_master.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/usart/plib_sercom0_usart.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "driver/i2c/drv_i2c.h"
 #include "system/time/sys_time.h"
@@ -77,6 +77,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 32000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -170,22 +173,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* I2C0 Driver Object */
