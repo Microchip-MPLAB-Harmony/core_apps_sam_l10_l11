8df29ab38a5d8cd7f1692f55b1bc704b8da31c0d
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-02 23:16:49 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_sam_l10_xpro
    MH3-50727
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/packs/ATSAML10E16A_DFP/pio/saml10e16a.h
+++ apps/system/time/sys_time_multiclient/firmware/src/packs/ATSAML10E16A_DFP/pio/saml10e16a.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2020-07-21T06:47:55Z */
+/* file generated from device description version 2020-11-19T07:18:13Z */
 #ifndef _SAML10E16A_GPIO_H_
 #define _SAML10E16A_GPIO_H_
 
