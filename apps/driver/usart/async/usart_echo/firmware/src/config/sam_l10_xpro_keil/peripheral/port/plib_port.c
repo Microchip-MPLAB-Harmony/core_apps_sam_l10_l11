3ba283dd6968eedc58b3db229cc71f38afcf57dd
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-05 17:31:32 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_echo_sam_l10_xpro_keil
    MH3-65552
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_l10_xpro_keil/peripheral/port/plib_port.c
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_l10_xpro_keil/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -79,10 +78,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[25] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[27] = 0x6;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[3] = 0x0;
    PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x22;
+   PORT_REGS->GROUP[0].PORT_PMUX[13] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
