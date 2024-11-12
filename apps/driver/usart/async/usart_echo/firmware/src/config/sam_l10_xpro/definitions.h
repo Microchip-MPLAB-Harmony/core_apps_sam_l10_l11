bfc44ad4f6c6fc2e084fe3b073d60f36b25af4b6
Pranjal Das <pranjal.das@microchip.com>
2024-11-12 15:26:36 +0530

    [DRV_USART] Regenerated demo for driver__usart__async__usart_echo__firmware__sam_l10_xpro.X
    MH3-104068

--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_l10_xpro/definitions.h
@@ -74,13 +74,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAML10E16A"
-#define DEVICE_ARCH			 "CORTEX-M23"
-#define DEVICE_FAMILY		 "SAML"
-#define DEVICE_SERIES		 "SAML10"
+#define DEVICE_NAME          "ATSAML10E16A"
+#define DEVICE_ARCH          "CORTEX-M23"
+#define DEVICE_FAMILY        "SAML"
+#define DEVICE_SERIES        "SAML10"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 32000000
+#define CPU_CLOCK_FREQUENCY 32000000U
 
 // *****************************************************************************
 // *****************************************************************************
--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_l10_xpro/initialization.c
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_l10_xpro/initialization.c
@@ -48,7 +48,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -81,9 +80,10 @@
 // *****************************************************************************
 // *****************************************************************************
 /* Following MISRA-C rules are deviated in the below code block */
-/* MISRA C-2012 Rule 11.1 */
-/* MISRA C-2012 Rule 11.3 */
-/* MISRA C-2012 Rule 11.8 */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 // <editor-fold defaultstate="collapsed" desc="DRV_USART Instance 0 Initialization Data">
 
 static DRV_USART_CLIENT_OBJ drvUSART0ClientObjPool[DRV_USART_CLIENTS_NUMBER_IDX0];
@@ -116,10 +116,10 @@ static const DRV_USART_INTERRUPT_SOURCES drvUSART0InterruptSources =
     .isSingleIntSrc                        = false,
 
     /* Peripheral interrupt lines */
-    .intSources.multi.usartTxCompleteInt   = (int32_t)SERCOM0_1_IRQn,
-    .intSources.multi.usartTxReadyInt      = (int32_t)SERCOM0_0_IRQn,
-    .intSources.multi.usartRxCompleteInt   = (int32_t)SERCOM0_2_IRQn,
-    .intSources.multi.usartErrorInt        = (int32_t)SERCOM0_OTHER_IRQn,
+   .intSources.multi.usartTxCompleteInt   = (int32_t)SERCOM0_1_IRQn,
+   .intSources.multi.usartTxReadyInt      = (int32_t)SERCOM0_0_IRQn,
+   .intSources.multi.usartRxCompleteInt   = (int32_t)SERCOM0_2_IRQn,
+   .intSources.multi.usartErrorInt        = (int32_t)SERCOM0_OTHER_IRQn,        
 };
 
 static const DRV_USART_INIT drvUsart0InitData =
@@ -198,6 +198,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
@@ -220,7 +221,6 @@ void SYS_Initialize ( void* data )
 
 	BSP_Initialize();
 
-
     /* MISRAC 2012 deviation block start */
     /* Following MISRA-C rules deviated in this block  */
     /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
@@ -239,9 +239,7 @@ void SYS_Initialize ( void* data )
 
 
     /* MISRAC 2012 deviation block end */
-
 }
-
 
 /*******************************************************************************
  End of File
